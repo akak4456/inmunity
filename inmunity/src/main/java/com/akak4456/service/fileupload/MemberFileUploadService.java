eadebb1f1af6cfab66d1b140e914f07690e3e821
akak4456 <akak4456@naver.com>
2020-08-17 16:38:02 +0900

    2020.08.17

--- inmunity/src/main/java/com/akak4456/service/fileupload/MemberFileUploadService.java
+++
@@ -1,5 +0,0 @@
-package com.akak4456.service.fileupload;
-
-public interface MemberFileUploadService {
-	public void putUserProfileInfoToDB(String uploadPath,String uploadFileName,String useremail);
-}
